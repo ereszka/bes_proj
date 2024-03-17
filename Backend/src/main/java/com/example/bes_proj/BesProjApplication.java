c56825ae9e4706fbce61da241ee1fabc789f3840
Eva Reszka <e.reszka@stud.elka.pw.edu.pl>
2024-03-17 14:52:17 +0100

    Init Springboot backend

---
+++ Backend/src/main/java/com/example/bes_proj/BesProjApplication.java
@@ -0,0 +1,13 @@
+package com.example.bes_proj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BesProjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BesProjApplication.class, args);
+	}
+
+}
